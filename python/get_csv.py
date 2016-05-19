c72df24f7886faac24f9b8c7a6eaf5f0290ccab8
e-stat-api <api_support@e-stat.go.jp>
2016-05-20 05:59:57 +0900

    edited codes with autopep8

--- python/get_csv.py
+++ python/get_csv.py
@@ -4,18 +4,12 @@ import sys
 sys.path.append('./')
 import e_Stat_API_Adaptor
 eStatAPI = e_Stat_API_Adaptor.e_Stat_API_Adaptor({
-	# 取得したappId
-	 'appId'	: '#appID#'
-	# データをダウンロード時に一度に取得するデータ件数
-	,'limit'	: '10000'
-	# next_keyに対応するか否か(非対応の場合は上記のlimitで設定した件数のみしかダウンロードされない)
-	# 対応時はTrue/非対応時はFalse
-	,'next_key'	: True
-	# 中間アプリの設置ディレクトリ
-	,'directory':'#Directory#'
-	# APIのバージョン
-	,'ver'		:'2.0'
+    'appId'	: '#appID#' # 取得したappId    
+    , 'limit'	: '10000'  # データをダウンロード時に一度に取得するデータ件数  
+    , 'next_key'	: True        # next_keyに対応するか否か(非対応の場合は上記のlimitで設定した件数のみしかダウンロードされない)# 対応時はTrue/非対応時はFalse
+    , 'directory': '#Directory#'      # 中間アプリの設置ディレクトリ
+    , 'ver'		: '2.0'       # APIのバージョン
 })
 # 0000030001をcsvの形式でダウンロード
-print 'id:'+sys.argv[1]
-print  eStatAPI.get_csv('get' , sys.argv[1])
+print 'id:' + sys.argv[1]
+print eStatAPI.get_csv('get', sys.argv[1])
