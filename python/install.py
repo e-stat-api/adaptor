7cb1792ddc57eb428eae0c274dbbaee215b28e12
e-stat-api <api_support@e-stat.go.jp>
2016-05-20 06:00:55 +0900

    edited codes with autopep8.

--- python/install.py
+++ python/install.py
@@ -4,19 +4,19 @@ import sys
 sys.path.append('./')
 import e_Stat_API_Adaptor
 eStatAPI = e_Stat_API_Adaptor.e_Stat_API_Adaptor({
-	# 取得したappId
-	 'appId'	: '#appID#'
-	# データをダウンロード時に一度に取得するデータ件数
-	,'limit'	: '10000'
-	# next_keyに対応するか否か(非対応の場合は上記のlimitで設定した件数のみしかダウンロードされない)
-	# 対応時はTrue/非対応時はFalse
-	,'next_key'	: True
-	# 中間アプリの設置ディレクトリ
-	,'directory':'#絶対パス#'
-	# APIのバージョン
-	,'ver'		:'2.0'
+    # 取得したappId
+    'appId'	: '#appId#'   
+    # データをダウンロード時に一度に取得するデータ件数
+    , 'limit'	: '10000'    
+    # next_keyに対応するか否か(非対応の場合は上記のlimitで設定した件数のみしかダウンロードされない)
+    # 対応時はTrue/非対応時はFalse
+    , 'next_key'	: True        
+   # 中間アプリの設置ディレクトリ
+    , 'directory': '#絶対パス# /foo/bar/'
+    # APIのバージョン        
+    , 'ver'		: '2.0'
 })
 # 全ての統計表IDをローカルにダウンロード
 print eStatAPI.load_all_ids()
-# ダウンロードした統計表IDからインデックスを作成 
+# ダウンロードした統計表IDからインデックスを作成
 print eStatAPI.build_statid_index()
